cee7e8aca8a0843d2c7d91789273fb52c32f44d4
yc-zzz <yc@Swift14.localdomain>
2026-01-28 17:07:02 +0800

    Implement level 1 echo

--- src/main/java/JohnChatBot.java
+++ src/main/java/JohnChatBot.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class JohnChatBot {
     public static void main(String[] args) {
         System.out.println("Hello! I'm John ChatBot");
         System.out.println("What can I do for you?");
+
+        Scanner myObj = new Scanner(System.in);
+        String line = "";
+
+        while (!line.equals("bye")) {
+            line = myObj.nextLine();
+
+            if (!line.equals("bye")) {
+                System.out.println(line);
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        myObj.close();
     }
 }
